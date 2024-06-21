e28603d565f7e36a3816a2c1b14937b4761ae34a
s24666 <s27466@pjwstk.edu.pl>
2024-06-21 23:06:48 +0200

    initial

---
+++ WebApplication1/WebApplication1/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Models.Movie> Movie { get; set; }
+    }
+}
